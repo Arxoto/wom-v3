2dd744a2c5384db92f29013c50eeefccad8faf04
Arxoto <1484663586@qq.com>
2026-09-26 21:49:56 +0800

    规划 系统命令的 todo 和 其他插件的 todo

--- src-tauri/src/builtin_plugins.rs
+++ src-tauri/src/builtin_plugins.rs
@@ -1,5 +1,17 @@
+//! 内建插件
+//! 
+//! todo 把 search 模块分离
+//! 
+//! todo 默认 scan 或独立应用插件： UWP / 开始菜单 / 注册表
+//! 
+//! todo 插件：浏览器书签
+//! 
+//! todo 插件：进程列表和 kill
+
 pub mod base;
 pub mod common;
+
+pub mod action_systems;
 
 pub mod action;
 
